560e3511aca514adf1dbaa50960eca1e246069d7
Irirwanirira <irijoseph41@gmail.com>
2022-10-18 20:33:58 +0200

    Modified index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+import render from './module/render';
+import scores from './module/score';
 import './styles.css';
+
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/amavubiGames/scores/';
+
+const refreshBtn = document.querySelector('.refresh');
+const form = document.querySelector('form');
+
+refreshBtn.addEventListener('click', () => {
+  render(url);
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const submitBtn = document.querySelector('.submit_btn');
+  const username = form.name.value;
+  const score = form.score.value;
+  scores(url, username, score);
+  form.reset();
+  submitBtn.disable = true;
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  render(url);
+});
